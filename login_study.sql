9a436c743b4eb4c1bee590e0c2aff14c1914e5f2
jinyoung <djawls111@gmail.com>
2025-04-29 17:15:40 +0900

    docker 시작

--- login_study.sql
+++ login_study.sql
@@ -1,20 +1,21 @@
 DROP DATABASE IF EXISTS login_study;
-DROP USER IF EXISTS 'login_study_dev'@'localhost';
+DROP USER IF EXISTS 'login_study_dev'@'%';
 CREATE DATABASE login_study CHAR SET utf8;
-CREATE USER 'login_study_dev'@'localhost' IDENTIFIED BY '1234';
-GRANT SELECT,INSERT,UPDATE,DELETE ON login_study.* TO 'login_study_dev'@'localhost';
+CREATE USER 'login_study_dev'@'%' IDENTIFIED BY '12345678';
+GRANT SELECT,INSERT,UPDATE,DELETE ON login_study.* TO  'login_study_dev'@'%';
+flush PRIVILEGES;
 USE login_study;
 CREATE TABLE users(
-    id VARCHAR(255) PRIMARY KEY,
-    pw VARCHAR(255) ,
-    picture VARCHAR(255) ,
-    oauth ENUM('GOOGLE','KAKAO', 'NAVER', 'GITHUB'),
-    role ENUM('GUEST','USER','MANAGER','ADMIN'),
-
-    name VARCHAR(255) NOT NULL
+                      id VARCHAR(255) PRIMARY KEY,
+                      pw VARCHAR(255),
+                      picture VARCHAR(255),
+                      oauth ENUM('GOOGLE','KAKAO','NAVER','GITHUB'),
+                      role ENUM('GUEST','USER','MANAGER','ADMIN'),
+                      name VARCHAR(255) NOT NULL
 );
+
 INSERT INTO users(id,pw,role,name)
-VALUES ('user1','$2a$10$HDA7deEKi9SlqcFSJriojOBQMDCNYDdwd0U87qRXhc5f6aQ5oveoS','USER','사용자'),
-       ('guest1','$2a$10$HDA7deEKi9SlqcFSJriojOBQMDCNYDdwd0U87qRXhc5f6aQ5oveoS','GUEST','게스트'),
-       ('manager1','$2a$10$HDA7deEKi9SlqcFSJriojOBQMDCNYDdwd0U87qRXhc5f6aQ5oveoS','MANAGER','매니저'),
-       ('admin1','$2a$10$HDA7deEKi9SlqcFSJriojOBQMDCNYDdwd0U87qRXhc5f6aQ5oveoS','ADMIN','관리자');
+VALUES ('user1','$2a$10$wlg3paJ3C6Yw7cy73AHg/OhuZzAzifmJyYhgzkCpq8ZyT4zbcJ3tu','USER','사용자'),
+       ('guest1','$2a$10$wlg3paJ3C6Yw7cy73AHg/OhuZzAzifmJyYhgzkCpq8ZyT4zbcJ3tu','GUEST','게스트'),
+       ('manager1','$2a$10$wlg3paJ3C6Yw7cy73AHg/OhuZzAzifmJyYhgzkCpq8ZyT4zbcJ3tu','MANAGER','매니저'),
+       ('admin1','$2a$10$wlg3paJ3C6Yw7cy73AHg/OhuZzAzifmJyYhgzkCpq8ZyT4zbcJ3tu','ADMIN','관리자');
